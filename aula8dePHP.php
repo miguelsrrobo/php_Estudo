32a412a4c8f67f3a4df588d7b7ca3df600fbb224
miguel <guegosilvestre67@gmail.com>
2021-02-06 12:28:43 -0300

    add aula8 de PHP

---
+++ aula8dePHP.php
@@ -0,0 +1,22 @@
+<?php
+
+$incrementador = 0;
+$valor_a_conparar = 8;
+$vetor_de_numeros = array($valor_a_conparar);
+
+do{
+    echo "ola";
+    echo " mundo<br/>";
+}while(++$incrementador <= $valor_a_conparar);
+
+?>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Aula 8 de PHP - Loop - Do Wihle </title>
+
+  </head>
+  <body> 
+  </body>
+</html>
