6c287659dffad9f60d3c5741d1c7e6b1fa4fda9c
miguel <guegosilvestre67@gmail.com>
2021-02-07 16:06:47 -0300

    add aula 9 de PHP

---
+++ curso de PHP/aula9dePHP.php
@@ -0,0 +1,22 @@
+<?php
+
+for($linha = 0; $linha < 5; $linha++)
+{
+    for($coluna = 0; $coluna < 5; $coluna++)
+    {
+        echo "a";
+    }
+    echo "<br/>";
+}
+
+?>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Aula 8 de PHP - Loop - For </title>
+
+  </head>
+  <body> 
+  </body>
+</html>
